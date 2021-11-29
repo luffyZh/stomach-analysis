4e5a66aad824320e7f70d9e2631d9b1472e3910b
wangtuo <wangtuo01@corp.netease.com>
2021-11-29 10:09:33 +0800

    update style

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,52 +1,48 @@
 <template>
   <div>
-
-
-      <div class="d3-pic">
-        <stomach></stomach>
+    <a-page-header
+    style="rgb(0, 0, 0)"
+    title="虚拟胃镜地图"/>
+    <div class="d3-pic">
+      <stomach></stomach>
+    </div>
+    <div>
+      <div style="text-align: center">
+        <a-button size="large" type="primary" @click="goStep2">
+          进入具体诊断
+        </a-button>
       </div>
-      <div>
-    <div style="text-align:center">
-      <a-button size="large" type="primary" @click="goStep2">
-      进入具体诊断
-    </a-button>
     </div>
-
-
-    </div>
-
   </div>
 </template>
 
 <script>
-import stomach from '@/components/stomach.vue'
+import stomach from "@/components/stomach.vue";
 export default {
-  name:'home',
-  components:{
-    stomach
+  name: "home",
+  components: {
+    stomach,
   },
-  data(){
-    return{
-
-    }
+  data() {
+    return {};
   },
   methods: {
     handleChange(value) {
       console.log(`selected ${value}`);
     },
-    goStep2(){
-      this.$router.push({path:'/step2'})
-    }
+    goStep2() {
+      this.$router.push({ path: "/step2" });
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-.home-content{
+.home-content {
   display: flex;
 }
-.d3-pic{
-  width:400px;
+.d3-pic {
+  width: 400px;
   height: 600px;
   background-color: bisque;
 }
--- src/components/step2.vue
+++ src/components/step2.vue
@@ -3,83 +3,76 @@
     <div class="header-top">
       <div class="title">学习胃镜流程</div>
       <div>
-    <a-button  @click="goHome">
-      首页
-    </a-button>
-      <a-button  @click="goHome">
-      上一页
-    </a-button>
-     <a-button @click="goStep3">
-      下一页
-    </a-button>
+        <a-button @click="goHome"> 首页 </a-button>
+        <a-button @click="goHome"> 上一页 </a-button>
+        <a-button @click="goStep3"> 下一页 </a-button>
       </div>
     </div>
     <div class="play_video">
       <video-player
-          class="video-player vjs-custom-skin"
-          ref="videoPlayer"
-          :options="playerOptions"
-          :playsinline="true"
-        >
-        </video-player>
+        class="video-player vjs-custom-skin"
+        ref="videoPlayer"
+        :options="playerOptions"
+        :playsinline="true"
+      >
+      </video-player>
     </div>
   </div>
 </template>
 
 <script>
-import 'video.js/dist/video-js.css';
-import 'vue-video-player/src/custom-theme.css';
-import videoSrc from '../assets/video.mp4'
-import { videoPlayer } from 'vue-video-player';
+import "video.js/dist/video-js.css";
+import "vue-video-player/src/custom-theme.css";
+import videoSrc from "../assets/video2.mp4";
+import { videoPlayer } from "vue-video-player";
 export default {
-  name:'step2',
-   components: {
+  name: "step2",
+  components: {
     videoPlayer,
   },
-  data(){
-    return{
-     playerOptions: {
-        height: '100%',
+  data() {
+    return {
+      playerOptions: {
+        height: "100%",
         autoplay: false,
         muted: true,
-        language: 'zh-CN',
+        language: "zh-CN",
         playbackRates: [0.7, 1.0, 1.5, 2.0],
         sources: [
           {
-            type: '',
-            src:videoSrc,
+            type: "",
+            src: videoSrc,
           },
         ],
-        aspectRatio: '16:9',
-        poster: '',
-        notSupportedMessage: '无法找到此课节视频源',
+        aspectRatio: "16:9",
+        poster: "",
+        notSupportedMessage: "无法找到此课节视频源",
       },
-    }
+    };
   },
-  methods:{
-    goHome(){
-      this.$router.push({path:'/'})
+  methods: {
+    goHome() {
+      this.$router.push({ path: "/" });
     },
-    goStep3(){
-      this.$router.push({path:'/step3'})
-    }
-  }
-}
+    goStep3() {
+      this.$router.push({ path: "/step3" });
+    },
+  },
+};
 </script>
 
 <style scoped>
-.header-top{
+.header-top {
   display: flex;
   align-items: center;
   justify-content: space-around;
   padding-top: 20px;
 }
-.title{
-  font-size:24px;
-
+.title {
+  font-size: 24px;
 }
 .play_video {
-     width:800px;
-     margin:30px auto;
-    }
+  width: 800px;
+  margin: 30px auto;
+}
 </style>
